40003ffef64ee29de031c4c0eeac9ef32817b5fa
kemannguyen <62680940+kemannguyen@users.noreply.github.com>
2022-04-26 20:45:35 +0200

    ValueNodes

---
+++ Editor/Nodes/ValueIntNode.cs
@@ -0,0 +1,42 @@
+
+using Celezt.DialogueSystem.Editor.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Celezt.DialogueSystem.Editor
+{
+    [CreateNode("Value/Int", "")]
+
+    public class ValueIntNode : DGNode
+    {
+        [SerializeField] private int _value = 0;
+        protected override void Awake()
+        {
+            this.AddStyleSheet(StyleUtility.STYLE_PATH + "Nodes/PropertyNode");
+            //
+            // Output Container
+            //
+            Port outputPort = this.InstantiatePort(Orientation.Horizontal, Direction.Output, Port.Capacity.Single, typeof(NumericPortType));
+            outputPort.portName = "Int";
+            outputContainer.Add(outputPort);
+
+            IntegerField valueTextField = new IntegerField()
+            {
+                value = _value,
+            };
+            valueTextField.RegisterValueChangedCallback(callback =>
+            {
+                _value = (callback.target as IntegerField).value;
+                hasUnsavedChanges = true;
+            });
+
+            extensionContainer.Add(valueTextField);
+
+            RefreshExpandedState();
+        }
+    }
+}
