9319efff503233ff135a87c7dd798883f2d7e086
Celezt <59172226+Celezt@users.noreply.github.com>
2022-04-27 12:35:34 +0200

    Changed control for BooleanProperty

--- Editor/Data/Properties/BooleanProperty.cs
+++ Editor/Data/Properties/BooleanProperty.cs
@@ -9,36 +9,19 @@ namespace Celezt.DialogueSystem.Editor
     [BlackboardProperty("bool")]
     public class BooleanProperty : BlackboardProperty<bool, ConditionPortType>
     {
-        private enum ConditionState
-        {
-            False,
-            True,
-        }
-
         public override VisualElement BuildController()
         {
-            var control = new EnumField(ConditionState.False)
+            var control = new Toggle
             {
-                value = ToEnum(_value),
+                value = _value,
             };
             control.RegisterValueChangedCallback(x =>
             {
-                Value = ToBool((ConditionState)x.newValue);
+                Value =  x.newValue;
                 hasUnsavedChanges = true;
             });
+
             return control;
         }
-
-        private static ConditionState ToEnum(bool value) => value switch
-        {
-            false => ConditionState.False,
-            true => ConditionState.True,
-        };
-
-        private static bool ToBool(ConditionState value) => value switch
-        {
-            ConditionState.True => true,
-            _ => false,           
-        };
     }
 }
